a51220eb50a0619fa18017d8f900227812ec0833
= <rpinosio@gmail.com>
2020-04-17 10:51:19 +0200

    fixes

--- start_container.ps1
+++ start_container.ps1
@@ -1,4 +1,4 @@
-param([string]$s="test", [string]$t="mounted_folder")
+param([string]$s, [string]$t)
 
 if ($s){
 docker run -i -p 8787:8787 -e PASSWORD=sandbox --name sandbox -v ${s}:/home/rstudio/${t} sandbox
