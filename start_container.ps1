a70ca49519529c207e828361c85bc33180f267dd
riccardopinosio <rpinosio@gmail.com>
2021-05-26 11:44:57 +0000

    mods

--- start_container.ps1
+++ start_container.ps1
@@ -8,10 +8,12 @@ if ($s){
         docker run --privileged -i -p 8787:8787 -p 8888:8888 -p 8889:8889 -p 2222:22 --name sandbox -h sandbox -v ${s}:/home/baseuser/${t} `
         --env RENV_PATHS_CACHE=${RENV_PATHS_CACHE_CONTAINER} `
         -v ${RENV_PATHS_CACHE_HOST}:${RENV_PATHS_CACHE_CONTAINER} `
+	--restart always `
         riccardopinosio/ds-sandbox-emacs
     } else {
         docker run --privileged -i -p 8787:8787 -p 8888:8888 -p 8889:8889 -p 2222:22 --name sandbox -h sandbox `
         --env RENV_PATHS_CACHE=${RENV_PATHS_CACHE_CONTAINER} `
         -v ${RENV_PATHS_CACHE_HOST}:${RENV_PATHS_CACHE_CONTAINER} `
+	--restart always `
         riccardopinosio/ds-sandbox-emacs
     }
